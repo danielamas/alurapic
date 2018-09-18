945ade927bd7c3539dd3ba60c91ac92feb29ecc6
Daniel Lamas <daniel.lamas@hexagonagriculture.com>
2018-09-17 23:55:48 -0300

    aula 10

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,43 +1,63 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http, $routeParams){ 
+    .controller('FotoController', function($scope, $resource, $routeParams, recursoFoto){ 
 
         $scope.foto = {};
         $scope.mensagem = '';
-    
-        if($routeParams.id) {
-            $http.get('/v1/fotos/' + $routeParams.id)
-            .success(function(foto) {
-                $scope.foto = foto;
-            })
-            .error(function(erro) {
-                $scope.mensagem = 'Não foi possível obter a foto!'
-            });
-        }
+        // var recursoFoto = $resource('/v1/fotos/:fotoId', null, { update : {method:'PUT'} });
+        recursoFoto.get({fotoId : $routeParams.id}, function(foto) {
+            $scope.foto = foto;
+        }, function(erro) {
+            $scope.mensagem = 'Não foi possível obter a foto!'
+        });
+
+        // if($routeParams.id) {
+        //     $http.get('/v1/fotos/' + $routeParams.id)
+        //     .success(function(foto) {
+        //         $scope.foto = foto;
+        //     })
+        //     .error(function(erro) {
+        //         $scope.mensagem = 'Não foi possível obter a foto!'
+        //     });
+        // }
 
         $scope.submeter = function() {
 
             if($scope.formulario.$valid) {
 
-                if($routeParams.id) {
-                    $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-                    .success(function(){
-                        $scope.mensagem = 'Foto alterada com sucesso!'
-                    })
-                    .error(function(error){
-                        $scope.mensagem = 'Erro! Foto não alterada!'
-                    });
-                }
+                recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function() {
+                    $scope.mensagem = 'Foto alterada com sucesso!'
+                }, function(error) {
+                    $scope.mensagem = 'Erro! Foto não alterada!'
+                });
 
-                $http.post('v1/fotos', $scope.foto)
-                .success(function() {
+                // if($routeParams.id) {
+                //     $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
+                //     .success(function(){
+                //         $scope.mensagem = 'Foto alterada com sucesso!'
+                //     })
+                //     .error(function(error){
+                //         $scope.mensagem = 'Erro! Foto não alterada!'
+                //     });
+                // }
+
+                recursoFoto.save($scope.foto, function() {
                     $scope.foto = {};
                     $scope.mensagem = 'Foto cadastrada com sucesso!';
-                })
-                .error(function(erro) {
+                }, function(erro) {
                     $scope.mensagem = 'Erro ao cadastrar!';
                     console.log(erro);
-                    
                 });
+
+                // $http.post('v1/fotos', $scope.foto)
+                // .success(function() {
+                //     $scope.foto = {};
+                //     $scope.mensagem = 'Foto cadastrada com sucesso!';
+                // })
+                // .error(function(erro) {
+                //     $scope.mensagem = 'Erro ao cadastrar!';
+                //     console.log(erro);
+                    
+                // });
             }
             
         }
---
+++ public/js/servicos/meus-servicos.js
@@ -0,0 +1,4 @@
+angular.module('meusServicos', ['ngResource'])
+    .factory('recursoFoto', function($resource) {
+        return $resource('/v1/fotos/:fotoId', null, { update : {method:'PUT'} });
+    });
